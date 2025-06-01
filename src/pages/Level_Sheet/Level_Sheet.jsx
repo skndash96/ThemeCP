0f1f5acd18e31721cbcacc568d49e8b6fba6d27c
Dash Skndash <dashskndash@gmail.com>
2025-06-01 16:59:57 +0530

    [feat] level sheet styles fixed

--- src/pages/Level_Sheet/Level_Sheet.jsx
+++ src/pages/Level_Sheet/Level_Sheet.jsx
@@ -4,7 +4,55 @@ import { LevelContext } from '../../context/LevelContext'
 
 const Level_Sheet = () => {
 
-    const { level } = useContext(LevelContext);
+    // Dummy data for development/demo purposes
+    const level = [
+        {
+            level: 1,
+            time: 10,
+            Performance: 1150,
+            P1: 1100,
+            P2: 1200,
+            P3: 1180,
+            P4: 1170
+        },
+        {
+            level: 2,
+            time: 12,
+            Performance: 1350,
+            P1: 1300,
+            P2: 1380,
+            P3: 1370,
+            P4: 1340
+        },
+        {
+            level: 3,
+            time: 15,
+            Performance: 1550,
+            P1: 1500,
+            P2: 1580,
+            P3: 1570,
+            P4: 1540
+        },
+        {
+            level: 4,
+            time: 18,
+            Performance: 1750,
+            P1: 1700,
+            P2: 1780,
+            P3: 1770,
+            P4: 1740
+        },
+        {
+            level: 5,
+            time: 20,
+            Performance: 1950,
+            P1: 1900,
+            P2: 1980,
+            P3: 1970,
+            P4: 1940
+        }
+    ];
+    // const { level } = useContext(LevelContext);
 
     const getBackgroundColor = (data) => {
         const rating = parseInt(data);
@@ -34,30 +82,33 @@ const Level_Sheet = () => {
     return (
         <div className='level-table'>
             <center><h1>Level Sheet</h1></center>
-            <div className='table-layout'>
-                <p>Level</p>
-                <div className='table-data'><p>Duration</p></div>
-                <div className='table-data'><p>Performance</p></div>
-                <div className='table-data'><p>P1 rating</p></div>
-                <div className='table-data'><p>P2 rating</p></div>
-                <div className='table-data'><p>P3 rating</p></div>
-                <div className='table-data'><p>P4 rating</p></div>
-                
-            </div> 
-            {
-                level.map((item,index) => (
-                    <div className='table-layout' key={index}>
-                        <div style={{fontFamily: 'monospace', fontWeight: 'bold'}}>{item.level}</div>
-                        <div className='table-data' style={{ backgroundColor: getBackgroundColor(item.Performance), border: '1px solid black', marginRight: '10px'}}><p style={{fontStyle:'italic'}}>{item.time} min</p></div>
-                        <div className='table-data' style={{ backgroundColor: getBackgroundColor(item.Performance), border: '1px solid black', marginRight: '10px'}}><p style={{fontStyle:'italic'}}>{item.Performance}</p></div>
-                        <div className='table-data' style={{ backgroundColor: getBackgroundColor(item.P1), border: '1px solid black'}}><p>{item.P1}</p></div>
-                        <div className='table-data' style={{ backgroundColor: getBackgroundColor(item.P2), border: '1px solid black'}}><p>{item.P2}</p></div>
-                        <div className='table-data' style={{ backgroundColor: getBackgroundColor(item.P3), border: '1px solid black'}}><p>{item.P3}</p></div>
-                        <div className='table-data' style={{ backgroundColor: getBackgroundColor(item.P4), border: '1px solid black'}}><p>{item.P4}</p></div>
-                    </div>
-                ))
-            }
-            
+            <table>
+                <thead>
+                    <tr>
+                        <td>Level</td>
+                        <td>Duration</td>
+                        <td>Performance</td>
+                        <td>P1 rating</td>
+                        <td>P2 rating</td>
+                        <td>P3 rating</td>
+                        <td>P4 rating</td>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {level.map((item, index) => (
+                        <tr key={index}>
+                            <td>{item.level}</td>
+                            <td style={{ backgroundColor: getBackgroundColor(item.Performance), border: '1px solid black', marginRight: '10px' }}><p style={{ fontStyle: 'italic' }}>{item.time} min</p></td>
+                            <td style={{ backgroundColor: getBackgroundColor(item.Performance), border: '1px solid black', marginRight: '10px' }}><p style={{ fontStyle: 'italic' }}>{item.Performance}</p></td>
+                            <td style={{ backgroundColor: getBackgroundColor(item.P1), border: '1px solid black' }}><p>{item.P1}</p></td>
+                            <td style={{ backgroundColor: getBackgroundColor(item.P2), border: '1px solid black' }}><p>{item.P2}</p></td>
+                            <td style={{ backgroundColor: getBackgroundColor(item.P3), border: '1px solid black' }}><p>{item.P3}</p></td>
+                            <td style={{ backgroundColor: getBackgroundColor(item.P4), border: '1px solid black' }}><p>{item.P4}</p></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
